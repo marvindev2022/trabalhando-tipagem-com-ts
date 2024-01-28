94f95153621af2fd53a3460e5176918405cf967f
marvindev2022 <mavirolero@gmail.com>
2024-01-28 17:48:09 -0300

    Add snakeCase to camelCase conversion utility

---
+++ snakeFromCamel.ts
@@ -0,0 +1,28 @@
+type SnakeCaseModel = {
+    client_name: string;
+    client_age: Date;
+    client_email: string;
+    client_address: string;
+    client_phone: string;
+  };
+  
+  type CamelCase<prop> = {
+    [K in keyof prop as CamelCaseKey<K & string >]: prop[K];
+  };
+  
+  type CamelCaseKey<T extends string > = T extends `${infer First}_${infer Rest}`
+    ? `${Uncapitalize<First>}${Capitalize<Rest>}`
+    : T;
+  
+  const teste: CamelCase<SnakeCaseModel> = {
+    clientName: 'teste',
+    clientAge: new Date(),
+    clientEmail: 'teste@gmail.com',
+    clientAddress: 'teste',
+    clientPhone: 'teste',
+  };
+  
+  
+  console.log(teste);
+
+  
