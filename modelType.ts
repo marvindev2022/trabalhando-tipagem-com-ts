32ca97d01cdfc0fb4bc711c057550f69142c7ca9
marvindev2022 <mavirolero@gmail.com>
2024-01-29 01:10:02 -0300

    Update user and admin model properties

--- modelType.ts
+++ modelType.ts
@@ -20,7 +20,7 @@ type DataBaseModel = {
   
   const outroTeste: CamelCaseConvert<DataBaseModel, 'user'> = {
     userName: 'teste',
-    userAge: new Date(),
+    userBirthDate: new Date(),
     userEmail: 'teste@gmail.com',
     userAddress: 'teste',
     userPhone: 'teste',
@@ -28,7 +28,7 @@ type DataBaseModel = {
   
   const ultimoTeste: CamelCaseConvert<DataBaseModel, 'admin'> = {
     adminAddress: 'teste',
-    adminAge: new Date(),
+    adminBirthDate: new Date(),
     adminEmail: 'teste@teste.com',
     adminName: 'teste',
     adminPhone: 'teste',
